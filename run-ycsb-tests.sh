f0718681d4592478d4c381b8e82aa0df3a04b8b9
Tanuj Mittal <tanuj.183@gmail.com>
2017-01-24 00:59:29 -0800

    Some changes

--- run-ycsb-tests.sh
+++ run-ycsb-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-num_threads=( 1 5 10 15 20 25 30 35 40 )
+num_threads=( 1 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 )
 for num in "${num_threads[@]}"
 do
 	"./start-nmon.sh"
@@ -11,7 +11,7 @@ do
 	echo "Waiting 120s for nmon to finish..."
 	sleep 120
 
-	path="./benchmarks/01.23.17.2/$num"
+	path="./benchmarks/01.24.17.2/$num"
 	mkdir -p $path
 	echo "Fetching ycsb-results from dsl3"
 	scp "dsl3:~/tanuj/ycsb-jdbc-binding-0.11.0/ycsb-results" $path
