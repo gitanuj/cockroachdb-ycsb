1758390ad7b5fa0f43e2048286b1b47da7b909ed
Tanuj Mittal <tanuj.183@gmail.com>
2017-02-21 00:25:22 -0800

    Add results and prepare for next one

--- run-ycsb-tests.sh
+++ run-ycsb-tests.sh
@@ -6,7 +6,7 @@
 #
 num_threads=( 1 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 )
 ycsb_machine="dsl3"
-benchmarks_dir="02.20.17.2"
+benchmarks_dir="02.20.17.3"
 for num in "${num_threads[@]}"
 do
 	"./start-nmon.sh"
@@ -30,4 +30,4 @@ done
 path="./benchmarks/$benchmarks_dir"
 echo "Fetching raft-leaders.csv"
 scp "$ycsb_machine:~/tanuj/ycsb-jdbc-binding-0.11.0/raft-leaders.csv" $path
-scp $ycsb_machine "rm -f tanuj/ycsb-jdbc-binding-0.11.0/raft-leaders.csv"
+ssh $ycsb_machine "rm -f tanuj/ycsb-jdbc-binding-0.11.0/raft-leaders.csv"
