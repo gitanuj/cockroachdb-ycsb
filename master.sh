b3701411569d2e87e9993cc8c2e74a8f196ea044
Tanuj Mittal <tanuj.183@gmail.com>
2017-02-26 04:31:41 -0800

    Add master script

---
+++ master.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# Usage: ./master.sh
+#
+
+benchmark_dir_prefix="02.26.17"
+read_types=( "0" "1" )
+workloads=( "uniform" "zipfian" )
+repetitions=1
+
+all_machines=( "dsl0" "dsl1" "dsl2" "dsl3" )
+crdb_machines=( "dsl0" "dsl1" "dsl2" )
+wdir="~/tanuj"
+ycsb_machine="dsl3"
+ycsb_wdir="~/tanuj/ycsb-jdbc-binding-0.11.0"
+
+function cleanup {
+	echo "Cleaning up YCSB client"
+	ssh $ycsb_machine "cd $ycsb_wdir; ./cleanup.sh"
+
+	echo "Cleaning up crdb servers"
+	for server in "${crdb_machines[@]}"
+	do
+		ssh $server "cd $wdir; ./cleanup.sh"
+	done
+}
+
+# $1: read_type
+# $2: workload
+# $3: count
+function run {
+	read_type=$1
+	workload=$2
+	count=$3
+
+	echo "Running for [read_type: $read_type, workload: $workload, count: $count]"
+
+	benchmark_dir="$benchmark_dir_prefix.$read_type.$workload.$count"
+	
+	# setup YCSB client
+	echo "Setting up YCSB client"
+	ssh $ycsb_machine "cd $ycsb_wdir; ./startup.sh"
+
+	# setup crdb servers
+	echo "Setting up crdb servers"
+	for server in "${crdb_machines[@]}"
+	do
+		ssh $server "cd $wdir; COCKROACH_READ_TYPE=$read_type; ./startup.sh" &
+	done
+	sleep 10
+
+	# init db
+	"./init-test-db.sh"
+
+	# load workload
+	echo "Loading $workload workload"
+	ssh "$ycsb_machine" "cd $ycsb_wdir; bin/ycsb load jdbc -P cockroachdb-ycsb/$workload -P cockroachdb-ycsb/cockroachdb.properties -s -cp cockroachdb-ycsb/bin/postgresql-9.4.1212.jre7.jar -threads 10"
+	sleep 10
+
+	# run benchmark
+	"./run-benchmark.sh" "$benchmark_dir $workload"
+
+	echo "Finished!"
+}
+
+# main
+for read_type in "${read_types[@]}"
+do
+	for workload in "${workloads[@]}"
+	do
+		for (( c=0; c<=$repetitions; c++ ))
+		do
+			cleanup
+			run $read_type $workload $c
+		done
+	done
+done
+cleanup
--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 #
-# Usage: ./run-benchmark.sh <benchmarks_dir>
+# Usage: ./run-benchmark.sh <benchmarks_dir> <workload>
 # Runs a ycsb benchmark for different num of threads
 # PS: Update workload file and the vars below
 #
 
 benchmarks_dir="$1"
-num_threads=( 1 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 )
+workload="$2"
+
+num_threads=( 20 )
 all_machines=( "dsl0" "dsl1" "dsl2" "dsl3" )
 crdb_machines=( "dsl0" "dsl1" "dsl2" )
 wdir="~/tanuj"
 ycsb_machine="dsl3"
 ycsb_wdir="~/tanuj/ycsb-jdbc-binding-0.11.0"
 
+# $1: ssh addr
+# $2: remote path
+# $3: local path
+function fetchUsingSsh {
+	echo "Fetching $remote_path from $ssh_addr"
+	mkdir -p $local_path
+	scp "$ssh_addr:$remote_path" "$local_path"
+	ssh "$ssh_addr" "rm -rf $remote_path"
+}
+
 # start record-raft-leaders.py
-# echo "Starting record-raft-leaders.py on $ycsb_machine"
-# ssh "$ycsb_machine" "cd $ycsb_wdir; python -u cockroachdb-ycsb/record-raft-leaders.py &> raft-leaders.csv &"
+echo "Starting record-raft-leaders.py on $ycsb_machine"
+ssh "$ycsb_machine" "cd $ycsb_wdir; python -u cockroachdb-ycsb/record-raft-leaders.py &> raft-leaders.csv &"
 
 for num in "${num_threads[@]}"
 do
@@ -28,36 +40,36 @@ do
 
 	# start ycsb
 	echo "Running YCSB for $num threads"
-	ssh "$ycsb_machine" "cd $ycsb_wdir; bin/ycsb run jdbc -P cockroachdb-ycsb/workload -P cockroachdb-ycsb/cockroachdb.properties -s -cp cockroachdb-ycsb/bin/postgresql-9.4.1212.jre7.jar -threads $num &> ycsb-results"
+	ssh "$ycsb_machine" "cd $ycsb_wdir; bin/ycsb run jdbc -P cockroachdb-ycsb/$workload -P cockroachdb-ycsb/cockroachdb.properties -s -cp cockroachdb-ycsb/bin/postgresql-9.4.1212.jre7.jar -threads $num &> ycsb-results"
 	echo "Finished running YCSB"
 
 	path="./benchmarks/$benchmarks_dir/$num"
 	mkdir -p $path
 
 	# fetch ycsb_results
-	"./fetch-using-ssh.sh" "$ycsb_machine" "$ycsb_wdir/ycsb-results" "$path"
+	fetchUsingSsh "$ycsb_machine" "$ycsb_wdir/ycsb-results" "$path"
 
 	# fetch *.nmon
 	for server in "${all_machines[@]}"
 	do
 		echo "Stopping nmon on $server"
 		ssh "$server" "kill -USR2 \$(ps -ef | grep 'nmon' | grep -v grep | awk '{ print \$2 }')"
-		"./fetch-using-ssh.sh" "$server" "$wdir/*.nmon" "$path/$server"
+		fetchUsingSsh "$server" "$wdir/*.nmon" "$path/$server"
 	done
 
 	# fetch data.dump
 	for server in "${crdb_machines[@]}"
 	do
-		"./fetch-using-ssh.sh" "$server" "$wdir/data.dump" "$path/$server"
+		fetchUsingSsh "$server" "$wdir/data.dump" "$path/$server"
 	done
 
 	echo ""
 done
 
 # stop record-raft-leaders.csv
-# echo "Stopping record-raft-leaders.py on $ycsb_machine"
-# ssh "$ycsb_machine" "kill -9 \$(ps -ef | grep 'record-raft-leaders' | grep -v grep | awk '{ print \$2 }')"
+echo "Stopping record-raft-leaders.py on $ycsb_machine"
+ssh "$ycsb_machine" "kill -9 \$(ps -ef | grep 'record-raft-leaders' | grep -v grep | awk '{ print \$2 }')"
 
 # fetch raft-leaders.csv
-# path="./benchmarks/$benchmarks_dir"
-# "./fetch-using-ssh.sh" "$ycsb_machine" "$ycsb_wdir/raft-leaders.csv" "$path"
+path="./benchmarks/$benchmarks_dir"
+fetchUsingSsh "$ycsb_machine" "$ycsb_wdir/raft-leaders.csv" "$path"
